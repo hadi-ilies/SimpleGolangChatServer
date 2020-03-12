8694cc1e501e25898fd260aa37bb08454a559329
hadi bereksi <hadi-ilies.bereksi-reguig@epitech.eu>
2020-03-12 19:43:11 +0100

    feat(client): Client package

---
+++ client/client.go
@@ -0,0 +1,46 @@
+package client
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+//connect to server
+func connect() net.Conn {
+	client, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal("Error during the connection :", err)
+	}
+	return client
+}
+
+//StartClient Start a simple client that will connect to the server and send text
+func StartClient() {
+	//connect client
+	client := connect()
+	// To create dynamic array
+	messages := make([]string, 0)
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		//buffer that store server's response
+		buf := make([]byte, 1024)
+		text := getLine(scanner)
+		if len(text) != 0 {
+			fmt.Println("message: \"", text, "\" sent !")
+			client.Write([]byte(text))
+			messages = append(messages, text)
+		} else {
+			break
+		}
+		// Read the incoming connection into the buffer.
+		resLen, err := client.Read(buf)
+		if err != nil {
+			log.Fatalln("Error has been read")
+		}
+		println("Server: ", string(buf), " nb byte read = ", resLen)
+	}
+	println(messages)
+}
---
+++ client/getline.go
@@ -0,0 +1,14 @@
+package client
+
+import (
+	"bufio"
+)
+
+//getLine get the line wrote by the client
+func getLine(scanner *bufio.Scanner) string {
+	// Scans a line from Stdin(Console)
+	scanner.Scan()
+	// Holds the string that scanned
+	line := scanner.Text()
+	return line
+}
