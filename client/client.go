966574ecbad467a242ef21a5e948ce146ae0003e
hadi bereksi <hadi-ilies.bereksi-reguig@epitech.eu>
2020-03-17 17:14:55 +0100

    feat(client): client can send/receive text from the server

--- client/client.go
+++ client/client.go
@@ -3,12 +3,14 @@ package client
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
+	"time"
 )
 
-//connect to server
+//connect: connect to server
 func connect(ipAddr string, port string) net.Conn {
 	client, err := net.Dial("tcp", ipAddr+":"+port)
 	if err != nil {
@@ -17,30 +19,46 @@ func connect(ipAddr string, port string) net.Conn {
 	return client
 }
 
-//StartClient Start a simple client that will connect to the server and send text
-func StartClient(ipAddr string, port string) {
-	//connect client
-	client := connect(ipAddr, port)
-	// To create dynamic array
-	messages := make([]string, 0)
+//sendText: get and send the text from the stdin
+func sendText(client net.Conn) {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		//buffer that store server's response
-		buf := make([]byte, 1024)
 		text := getLine(scanner)
 		if len(text) != 0 {
-			fmt.Println("message: \"", text, "\" sent !")
+			//the text is sent to the server here !
 			client.Write([]byte(text))
-			messages = append(messages, text)
 		} else {
 			break
 		}
-		// Read the incoming connection into the buffer.
-		resLen, err := client.Read(buf)
-		if err != nil {
-			log.Fatalln("Error has been read")
-		}
-		println("Server: ", string(buf), " nb byte read = ", resLen)
 	}
-	println(messages)
+}
+
+//StartClient : Start a simple client that will connect to the server and send text
+func StartClient(ipAddr string, port string) {
+	//connect client
+	client := connect(ipAddr, port)
+	//async text input
+	go sendText(client)
+	for {
+		// Read the incoming connection into the buffer.
+		client.SetReadDeadline(time.Now().Add(30 * time.Microsecond))
+		buf := make([]byte, 1024)
+		_, err := client.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				// io.EOF, etc
+				println("EOF detected !!!")
+				return
+			} else if err.(*net.OpError).Timeout() {
+				//println("TIMEOUT")
+				// no status msgs
+				// note: TCP keepalive failures also cause this; keepalive is on by default
+				continue
+			}
+			fmt.Println("Error reading:", err.Error())
+		}
+		//print message sent by the server
+		println(string(buf))
+	}
 }
