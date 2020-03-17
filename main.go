8bd8ab4f418f1b111829f668aa6c0ee2bb407742
hadi bereksi <hadi-ilies.bereksi-reguig@epitech.eu>
2020-03-17 18:29:09 +0100

    feat(main): argv management and usage

--- main.go
+++ main.go
@@ -7,19 +7,41 @@ import (
 	"./server"
 )
 
+func usage(exitValue int) {
+	println("USAGE")
+	println("\tgo run main.go [options]")
+	println("DESCRIPTION")
+	println("\tThis Program is a very simple chat Server that works following a room concept, for instance the server create/manage rooms and users connect to it.")
+	println("\tEach users can connect to a room or join one if there is an available spot. The server checks if there is an available spot in active rooms, if not, it will create a new one")
+	println("OPTIONS")
+	println("\t-S, --server <[ipaddr], port>")
+	println("\t\tstart server using the ip and port pass in argv")
+	println("\t-C, --client <ipaddr, port>")
+	println("\t\tstart client on the ip and port pass in argv")
+	println("\t-h, --help")
+	println("\t\tDisplay the program usage")
+	os.Exit(exitValue)
+}
+
 func main() {
 	if len(os.Args) < 3 {
-		os.Exit(1)
+		usage(1)
 	} else {
-		if os.Args[1] == "server" {
+		if os.Args[1] == "--server" || os.Args[1] == "-S" {
 			println("start server here")
 			if len(os.Args) == 3 {
-				server.StartServer(os.Args[2])
+				//create server. The IPV4 will be automaticly chosen whether we have internet or not
+				server := server.NewServer("tcp")
+				//start server on the port chosen in argv
+				server.Start(os.Args[2])
 			} else {
-				server.StartServer(os.Args[2], os.Args[3])
+				//create server with a specific IPV4
+				server := server.NewServer("tcp", os.Args[2])
+				//start server on the port chosen in argv
+				server.Start(os.Args[3])
 			}
 		}
-		if os.Args[1] == "client" && len(os.Args) == 4 {
+		if (os.Args[1] == "--client" && len(os.Args) == 4) || (os.Args[1] == "-C" && len(os.Args) == 4) {
 			println("Start client here")
 			client.StartClient(os.Args[2], os.Args[3])
 		}
