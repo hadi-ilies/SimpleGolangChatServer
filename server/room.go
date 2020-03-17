4bcc59ecabbfa09c9720ea5713171d35d46a4057
hadi bereksi <hadi-ilies.bereksi-reguig@epitech.eu>
2020-03-17 17:16:56 +0100

    feat(server): add methods for server and room struct

--- server/room.go
+++ server/room.go
@@ -1,12 +1,117 @@
 package server
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
 //room is a kind of channel that allow users to make group conv
 type room struct {
-	NbChatter  uint8
-	MessagesDB []string
+	nbMaxChatter uint8
+	chatters     []net.Conn
+	messagesDB   []string
 }
 
 //NewRoom is a room constructor
 func newRoom(nbChatter uint8) room {
-	return room{NbChatter: nbChatter}
+	return room{nbMaxChatter: nbChatter}
+}
+
+//isFull: check whether the room is full or not
+func (room *room) isFull() bool {
+	if uint8(len(room.chatters)) < room.nbMaxChatter {
+		return false
+	}
+	return true
+}
+
+//close: close the room by closing all chatters conn
+func (room *room) close() {
+	//note the _ (ignored) is the index and chatter the element
+	for _, chatter := range room.chatters {
+		chatter.Close()
+	}
+}
+
+//getChatterID: get client index from chatters
+func (room *room) getChatterID(target *net.Conn) int {
+	for index, chatter := range room.chatters {
+		if chatter == *target {
+			return index
+		}
+	}
+	return -1 //chatter does not exist
+}
+
+//deleteChatter: allow to delete/eject client from the room
+func (room *room) deleteChatter(target *net.Conn) {
+	chatterID := room.getChatterID(target)
+	if chatterID == -1 {
+		log.Fatal("Error chatter doesn't exist !!!")
+	}
+	//close chatter
+	room.chatters[chatterID].Close()
+	//eject chatter from the room
+	room.chatters = append(room.chatters[:chatterID], room.chatters[chatterID+1:]...) //chatterID is the index of the element to be removed
+}
+
+//addChatter: allow to add a new client in a room
+func (room *room) addChatter(newChatter net.Conn) {
+	if uint8(len(room.chatters)) < room.nbMaxChatter {
+		room.chatters = append(room.chatters, newChatter)
+	} else {
+		println("Too many chatters in this Room ! Create another one or increase the maximum number of chatters ;)")
+	}
+}
+
+//this function detect whether read has detected an EOF or just has 'timeouted' or there is no error
+//@return bool, bool: the first bool is eof detection and the seconde timeout detection
+func (room *room) handleReadError(err error) (bool, bool) {
+	if err != nil {
+		if err == io.EOF {
+			// io.EOF, etc
+			println("EOF detected !!!")
+			return true, false //tmp
+		} else if err.(*net.OpError).Timeout() {
+			//this allow us to set an "no-blocking read"
+			// no status msgs
+			// note: TCP keepalive failures also cause this; keepalive is on by default
+			return false, true
+		}
+		fmt.Println("Error reading:", err.Error())
+	}
+	return false, false
+}
+
+//sendToAll: send text to all clients
+func (room *room) sendToAll(text string) {
+	for i := range room.chatters {
+		room.chatters[i].Write([]byte(text))
+	}
+}
+
+//sendToAllExcept: send text to all clients except the ones passed in arg
+func (room *room) sendToAllExcept(text string, chattersID []uint8) {
+	for i := range room.chatters {
+		isExcepted := false
+
+		for _, chatterID := range chattersID {
+			if uint8(i) == chatterID {
+				isExcepted = true
+			}
+		}
+		if !isExcepted {
+			room.chatters[i].Write([]byte(text))
+		}
+	}
+}
+
+//sendTo: send text to all chatters specified in param
+//Note We considerate that the chattersId slice is always good
+func (room *room) sendTo(text string, chattersID []uint8) {
+	for _, chatterID := range chattersID {
+		room.chatters[chatterID].Write([]byte(text))
+	}
 }
--- server/server.go
+++ server/server.go
@@ -2,61 +2,117 @@ package server
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"strconv"
+	"time"
 )
 
-func createListenner(port string, ipAddr ...string) net.Listener {
-	//init server
-	const connType string = "tcp"
+//Server is a struct that handle and manage all the chatrooms
+type Server struct {
+	port           string
+	ipAddr         string
+	connectionType string
+}
+
+//NewServer is the server's constructor
+func NewServer(connectionType string, ipAddr ...string) Server {
+	//get my IP addr
 	ip := getOutboundIP().String()
 	if len(ipAddr) > 0 {
 		ip = ipAddr[0]
 	}
 	println("IP equal: ", ip)
-	// Listen for incoming connections.
-	l, err := net.Listen(connType, ip+":"+port)
+	return Server{ipAddr: ip, connectionType: connectionType}
+}
+
+func (server *Server) createListenner(port string) net.Listener {
+	//create listenner.
+	l, err := net.Listen(server.connectionType, server.ipAddr+":"+port)
 	if err != nil {
 		fmt.Println("Error listening:", err.Error())
 		os.Exit(1)
 	}
+	//save the current port
+	server.port = port
 	return l
 }
 
-//StartServer allow to start/run a server in tcp on the localhost:8080
-func StartServer(port string, ipAddr ...string) {
-	//init server
-	l := createListenner(port, ipAddr...)
+//Start allow to start/run a server in tcp on the localhost:8080
+func (server *Server) Start(port string) {
+	//create listenner
+	l := server.createListenner(port)
 	//close server when server stop
 	defer l.Close()
-
+	//define nb max chatter per rooms
+	const nbMaxChatterInRoom = uint8(3)
+	//create a slice of room/channel
+	var rooms []room
 	//Listen for an incoming connection.
 	for {
+		clientHasFoundRoom := false
 		conn, err := l.Accept()
 		if err != nil {
-			println("Error accepting: ", err.Error())
-			os.Exit(1)
+			log.Fatal("Error accepting: ", err.Error())
 		}
-		println(conn)
-		// Handle connections in a new goroutine.
-		go createRoom(conn)
+		println("A new client has been detected")
+		//add client in a free Room
+		for index := range rooms {
+			println("nb chatters BEFORE = ", len(rooms[index].chatters))
+			//todo this cond is not required we can use the return value of addChatter but I am too lazy
+			if rooms[index].isFull() {
+				continue
+			} else {
+				rooms[index].addChatter(conn)
+				rooms[index].sendToAll("\nA new client has join the room\n")
+				clientHasFoundRoom = true
+			}
+			println("nb chatters AFTER = ", len(rooms[index].chatters))
+		}
+		//if there is no free room, I create a new one
+		if !clientHasFoundRoom {
+			println("Create a new room")
+			//create a new room
+			rooms = append(rooms, newRoom(nbMaxChatterInRoom))
+			//get the last index of the slice
+			newRoomIndex := len(rooms) - 1
+			//add client in new room
+			rooms[newRoomIndex].addChatter(conn)
+			//load room in a goroutine (thread)
+			go manageRoom(&rooms[newRoomIndex])
+		}
 	}
 }
 
-func createRoom(conn net.Conn) {
-	defer conn.Close()
+//todo use channels instead of pointers
+//manageRoom: handle and manage all the room logic. In our case the client send a message in the room and the server resend the message for all other room's clients
+func manageRoom(room *room) {
+	defer room.close()
+	println("Start to manage room")
 	for {
 		// Make a buffer to hold incoming data.
 		buf := make([]byte, 1024)
 		// Read the incoming connection into the buffer.
-		resLen, err := conn.Read(buf)
-		if err != nil {
-			fmt.Println("Error reading:", err.Error())
+		for i, chatter := range room.chatters {
+			//todo find a way to set read in non blocking mode WITHOUT timeout
+			chatter.SetReadDeadline(time.Now().Add(1 * time.Microsecond))
+			//the first return value of read correspond to the number of byte read
+			_, err := chatter.Read(buf)
+			eof, timeout := room.handleReadError(err)
+			if eof {
+				room.deleteChatter(&chatter)
+				room.sendToAll("\nClient " + strconv.Itoa(i+1) + " has quit the room\n")
+				continue
+			}
+			if timeout {
+				continue
+			}
+			//send text to all clients except the one who sent the message to the server
+			room.sendToAllExcept("\nClient "+strconv.Itoa(i+1)+": "+string(buf), []uint8{uint8(i)})
+			//resend the text to the client in order to "save" his messages
+			//Note/todo: it better to display the text directly in the client and not resend the message to the client
+			room.sendTo("\nMe: "+string(buf), []uint8{uint8(i)})
 		}
-		//print message sent by the client
-		println("Client: ", string(buf), " nb byte read = ", resLen)
-		// Send a response back to person contacting us.
-		conn.Write([]byte("Message received."))
-		// Close the connection when you're done with it.
 	}
 }
